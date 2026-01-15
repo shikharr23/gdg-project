f593e3107f9c17013349ea1c0f33a488c55a0cd6
Shikhar <shikharchangotra01@gmail.com>
2026-01-16 02:10:23 +0530

    formatted

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+
 dotenv.config();
 
 const app = express();
@@ -10,19 +12,24 @@ import subjectRouter from "./routes/subject.js";
 import attendanceRouter from "./routes/attendance.js";
 import bunkRouter from "./routes/bunkAnalysis.js";
 import dashboardRouter from "./routes/dashboard.js";
-const simulatorRouter = require("./routes/leaveSimulator");
+// const simulatorRouter = require("./routes/leaveSimulator");
 
 app.use("/subjects", subjectRouter);
 app.use("/attendance", attendanceRouter);
 app.use("/bunk-analysis", bunkRouter);
-app.use("/leave-simulator", simulatorRouter);
+// app.use("/leave-simulator", simulatorRouter);
 app.use("/dashboard", dashboardRouter);
 
 app.get("/health", (req, res) =>
   res.json({ ok: true, time: new Date().toISOString() })
 );
 
-const port = process.env.PORT || 6767;
-app.listen(port, () => console.log(`Server listening on ${port}`));
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("Connected to MongoDB");
+  app.listen( process.env.PORT , () => console.log(`Server listening on ${process.env.PORT}`));
+}
+main();
+
 
 export default app;
